efcb90d8c3d1d36f931020bd9d4fe3f52e2225c9
hugostanley <stanleyhugo06@gmail.com>
2022-07-29 15:46:12 +0800

    Update installed servers

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 local configs = require"nvim-treesitter.configs"
 configs.setup{
-	ensure_installed = {"c","ruby","lua","javascript","typescript","css","scss","html"},
+	ensure_installed = {"c","ruby","lua","javascript","typescript","css","scss","html","json","json5","markdown","markdown_inline", "jsdoc", "fish"},
 	highlight = {
 		enable = true,
 	},
